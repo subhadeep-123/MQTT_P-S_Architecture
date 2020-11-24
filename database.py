e06bc1e5b0a168c0d1697995137bf6ec85890497
subhadeep-123 <subhadeep762@gmail.com>
2020-11-24 23:14:49 +0530

    Added DB Functionality

--- database.py
+++ database.py
@@ -1,6 +1,18 @@
 import sqlite3
+conn = sqlite3.connect('Example.db')
+c = conn.cursor()
 
-print("Connected to the datebase baby!!")
+# Create table
+# c.execute('''CREATE TABLE stocks
+#  (date text, trans text, symbol text, qty real, price real)''')
 
-print("Hello World")
+# Insert a row of data
+# c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+c.execute("INSERT INTO stocks VALUES ('2007-02-01','BUY','RHAT',100,35.14)")
 
+# Save (commit) the changes
+conn.commit()
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+conn.close()
